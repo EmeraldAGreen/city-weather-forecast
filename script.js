5176fd5ba1a0bfdf0edc19d09b43eac55ee2d578
Emerge <egreengirl1@gmail.com>
2022-02-01 20:15:29 -0700

    created API key for Geocoding API

---
+++ script.js
@@ -0,0 +1,7 @@
+var apiKey = "3c1af0981b3683839898c3c198395aa7";
+var city = "";
+var country = "";
+var state = "";
+
+var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=city,state,country&limit=5&appid=apiKey"
+
